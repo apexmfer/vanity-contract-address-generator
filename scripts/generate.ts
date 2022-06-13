fe04984a03777a2e86185f986544fc333d8ce41a
admazzola <admazzola@gmail.com>
2022-06-12 20:24:39 -0400

    init

---
+++ scripts/generate.ts
@@ -0,0 +1,41 @@
+
+import {Wallet, utils} from 'ethers'
+ 
+function genWallet(){
+
+
+    let newWallet = Wallet.createRandom()
+
+    const transactionCount = 0 
+    
+    const futureAddress = utils.getContractAddress({
+        from: newWallet.address,
+        nonce: transactionCount
+      })
+
+    return {
+        wallet: newWallet,
+        futureAddress: futureAddress
+    }
+
+}
+
+function init(){
+
+    console.log('Searching for a match...')
+ 
+    let computed 
+    while(true){
+
+        computed = genWallet();
+
+        if(computed.futureAddress.startsWith("0x00000")){ 
+            break; 
+        }
+    }
+
+    console.log('found a match ', JSON.stringify(computed))
+
+}
+
+init()
